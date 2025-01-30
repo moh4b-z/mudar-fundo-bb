3efa7a3d38c77808e565d7dc2938fd8dc166ad04
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-01-30 11:27:22 -0300

    Finalização do JS

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-"use stict"
+"use strict"
 
 const botaoTrocarImg = document.getElementById('trocar-fundo')
+const body = document.getElementById('body')
+let numero = 0
+let  urlImg = ""
 
 function trocarImg(){
-    console.log('oi')
+    if(numero === 5){
+        numero = 0
+    }
+    numero++
+    urlImg = `url(./imgs/sekiro${numero}.jpg)`
+    document.documentElement.style.setProperty('--fundo', urlImg)
 }
 
 botaoTrocarImg.addEventListener('click', trocarImg)
