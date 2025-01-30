ddb0f9386ead71a8f3079eaa78dcac24dc85c907
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-01-30 10:59:11 -0300

    criação do JS e alteração do CSS

---
+++ main.js
@@ -0,0 +1,9 @@
+"use stict"
+
+const botaoTrocarImg = document.getElementById('trocar-fundo')
+
+function trocarImg(){
+    console.log('oi')
+}
+
+botaoTrocarImg.addEventListener('click', trocarImg)
